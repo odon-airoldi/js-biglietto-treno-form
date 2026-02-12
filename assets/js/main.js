c481537be87c87ba18f7a4ad338462b460abee97
odon-airoldi <odon.airoldi@gmail.com>
2026-02-12 17:11:48 +0100

    completed first milestone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,72 @@
-const nameEl = document.getElementById('name');
+/* Descrizione:
+Scrivere un programma che chieda all’utente:
+- il numero di chilometri da percorrere
+- età del passeggero
 
-const buttonEl = document.querySelector('button');
-
-const formEl = document.querySelector('form');
-
-
-buttonEl.addEventListener('click', function() {
-    console.log(nameEl.value);
-})
-
-formEl.addEventListener('submit', function(e) {
-    e.preventDefault()
-    console.log(nameEl.value);
-    
-}
-)
-
-
-console.log(nameEl, nameEl.value)
-
-
-
-
-/*
-
-Il programma dovrà chiedere all'utente
-- il numero di chilometri che vuole percorrere
-- e l'età del passeggero.
-
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65
-- L'output del prezzo finale va messo fuori in forma umana
-    (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-TIP:
-per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
-100km, 10 anni => prezzo corretto:  €16.80
-100km, 70 anni => prezzo corretto: €12.60
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica:
+usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
+La risposta finale (o output) sarà anch’essa da scrivere in console.
 
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo
+un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina
+(il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+MILESTONE 3:
+Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile,
+raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
 */
 
 
-const prezzoAlKm = 0.21;
-const distanza = Number(prompt('Quanti KM devi percorrere?'));
-const eta = prompt('Quanti anni hai?');
-const prezzoTratta = distanza * prezzoAlKm
-let prezzoBiglietto;
 
-if ( eta < 18 ) {
+const formEl = document.querySelector('form');
+const kmrouteEl = document.getElementById('kmroute');
+const userageEl = document.getElementById('userage');
 
-    prezzoBiglietto = prezzoTratta - prezzoTratta * 0.2
 
-} else if ( eta >= 65) {
 
-    prezzoBiglietto = prezzoTratta - prezzoTratta * 0.4
+formEl.addEventListener('submit', function(e){
 
-} else {
+    e.preventDefault()
 
-    prezzoBiglietto = prezzoTratta
+    
+    const kmrouteElvalue = kmrouteEl.value;
+    const userageElvalue = userageEl.value;
+    const prezzoAlKm = 0.21;
+    const prezzoTratta = kmrouteElvalue * prezzoAlKm;
 
-}
+    let prezzoBiglietto;
 
-console.log(`Totale ${prezzoBiglietto.toFixed(2)}€`);
+    if ( userageElvalue < 18 ) {
+
+        prezzoBiglietto = prezzoTratta - prezzoTratta * 0.2
+
+    } else if ( userageElvalue >= 65) {
+
+        prezzoBiglietto = prezzoTratta - prezzoTratta * 0.4
+
+    } else {
+
+        prezzoBiglietto = prezzoTratta
+
+    }
+
+    console.log(`Distanza(km):${kmrouteElvalue} Età(anni):${userageElvalue} Totale(€):${prezzoBiglietto.toFixed(2)}`);
+
+});
+
+
+
+
+
+
+
+
+
